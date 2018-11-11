61db298a0ed27c3d112f96ee46f2636d4b621aa3
Amjad Al-Sawafta <atubasi@amazon.com>
2018-11-11 02:34:34 +0200

    define Keys struct

---
+++ NewsApp/Preferences/Keys.swift
@@ -0,0 +1,14 @@
+//
+//  Keys.swift
+//  NewsApp
+//
+//  Created by Al-sawafta, Amjad on 11/11/18.
+//  Copyright © 2018 AlFuttam. All rights reserved.
+//
+
+import UIKit
+
+struct Keys {
+    static let kKeyBaseUrl = "BASE_URL"
+    static let KKeyAPIKey  = "API_KEY"
+}
